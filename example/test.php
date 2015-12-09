0804d86998a24da37701455d335a04091a02a6af
fifths <ligangmingx@gmail.com>
2015-12-09 15:11:03 +0800

    test

--- example/test.php
+++ example/test.php
@@ -6,14 +6,11 @@
 
 require __DIR__."/../vendor/autoload.php";
 
-/*$pinyin=new Pinyin\Pinyin();*/
+$pinyin=new Pinyin\Pinyin();
 
-/*$str1=$pinyin->GetPinyin("宁波",0);
+$str1=$pinyin->GetPinyin("宁波",0);
 echo $str1,'<br />';
 $str2=$pinyin->GetPinyin("宁波",1);
 echo $str2,'<br />';
 $str3=$pinyin->GetPinyin("囧字测试",0);
-echo $str3,'<br />';*/
-
-$test=new \Pinyin\Abc();
-$str1=$test->GetPinyin("宁");
+echo $str3,'<br />';

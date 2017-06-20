265b738c9b3f1c91e30f9e897ce5fcafed5a3367
fifths <ligangmingx@gmail.com>
2017-06-20 13:54:55 +0800

    add class pinyinmini

--- example/test.php
+++ example/test.php
@@ -6,11 +6,17 @@
 
 require __DIR__."/../vendor/autoload.php";
 
-$pinyin=new Pinyin\Pinyin();
+/*$pinyin=new Pinyin\Pinyin();
 
 $str1=$pinyin->GetPinyin("宁波",0);
 echo $str1,'<br />';
 $str2=$pinyin->GetPinyin("宁波",1);
 echo $str2,'<br />';
 $str3=$pinyin->GetPinyin("囧字测试",0);
-echo $str3,'<br />';
+echo $str3,'<br />';*/
+
+
+$pinyin=new Pinyin\PinyinMini();
+var_dump($pinyin->utf8_to('囧字测试'));
+var_dump($pinyin->utf8_to('囧字测试',1));
+var_dump($pinyin->to_first('字测试'));
